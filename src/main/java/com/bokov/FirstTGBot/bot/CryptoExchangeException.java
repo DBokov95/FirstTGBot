12417cf80b95da4241f0bdfe9640b882078add7e
DBokov95 <dbokow.db@gmail.com>
2024-03-28 15:37:10 +0200

    Master

--- src/main/java/com/bokov/FirstTGBot/bot/CryptoExchangeException.java
+++
@@ -1,8 +0,0 @@
-package com.bokov.FirstTGBot.bot;
-
-public class CryptoExchangeException extends RuntimeException {
-    public CryptoExchangeException(String message) {
-        super(message);
-    }
-}
-
